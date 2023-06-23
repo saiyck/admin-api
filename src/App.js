2a6062e6a68bb738fe6551df9e374f2b5574fe0c
saiyck <saiyckpm@gmail.com>
2023-06-23 16:58:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {createTheme,ThemeProvider} from '@mui/material/styles'
+import MainScreen from './components/MainScreen';
+
+
+const defaultTheme = createTheme();
 
 function App() {
+  // const queryParameters = new URLSearchParams(window.location.search)
+  // const type = queryParameters.get("type")
+  // const name = queryParameters.get("name")
   return (
+    <ThemeProvider theme={defaultTheme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainScreen/>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/MainScreen.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Snackbar from '@mui/material/Snackbar';
+import './MainScreen.css';
+export default function MainScreen() {
+    const [open, setOpen] = React.useState(false);
+    const [copy, setCopy] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleCopyText = (text) => {
+        navigator.clipboard.writeText(text)
+        setCopy(true)
+        handleClose()
+    }
+    return (
+        <>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Admin Portal
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 6,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h4"
+                            variant="h4"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            Create Online Test
+                        </Typography>
+                        <Card sx={{ minWidth: 275 }}>
+                            <CardContent>
+                                <TextField
+                                    id="outlined-multiline-size-large"
+                                    label="System prompt"
+                                    multiline
+                                    rows={6}
+                                    placeholder="Please enter prompt message"
+                                    fullWidth
+                                />
+                                <CardActions sx={{ mt: 2 }}>
+                                    <Button onClick={handleOpen} variant='contained' size="small">Create Test Link</Button>
+                                </CardActions>
+                            </CardContent>
+                        </Card>
+                    </Container>
+                </Box>
+                <Modal
+                    open={open}
+                    // onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Your test link has been created
+                        </Typography>
+                        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                            please click on below url to copy
+                        </Typography>
+                        <Button onClick={() => handleCopyText('http://localhost:3000?id=abhdc54bdjd32ndjjd333')} size="small">http://localhost:3000?id=abhdc54bdjd32ndjjd333</Button>
+                    </Box>
+                </Modal>
+                <Snackbar
+                    open={copy}
+                    onClose={() => setCopy(false)}
+                    autoHideDuration={2000}
+                    message="Copied to clipboard"
+                />
+            </main>
+        </>
+    )
+}
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50vh',
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
